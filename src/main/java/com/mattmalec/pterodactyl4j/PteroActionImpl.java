d62a9bdcabb9c457766fb33172f5207f1cbcd699
HydroPage90 <alejolaverde0927@gmail.com>
2020-12-04 20:06:16 -0600

    Add a thread pool to PteroActionImpl?

--- src/main/java/com/mattmalec/pterodactyl4j/PteroActionImpl.java
+++ src/main/java/com/mattmalec/pterodactyl4j/PteroActionImpl.java
@@ -2,57 +2,53 @@ package com.mattmalec.pterodactyl4j;
 
 import com.mattmalec.pterodactyl4j.entities.PteroAction;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class PteroActionImpl<T> implements PteroAction<T>
-{
+import static java.util.concurrent.Executors.newCachedThreadPool;
 
-	private final Supplier<? extends T> execute;
+public class PteroActionImpl<T> implements PteroAction<T> {
+    private static final ThreadGroup threadGroup = new ThreadGroup("PtaeroAction Callback Workers");
+    static final ExecutorService callbackPool = newCachedThreadPool(runnable -> new Thread(threadGroup, runnable));
 
-	public static <T> PteroActionImpl<T> onExecute(Supplier<? extends T> execute)
-	{
-		return new PteroActionImpl<>(execute);
-	}
+    private final Supplier<? extends T> execute;
 
-	private PteroActionImpl(Supplier<? extends T> execute) {
-		this.execute = execute;
-	}
+    public static <T> PteroActionImpl<T> onExecute(Supplier<? extends T> execute) {
+        return new PteroActionImpl<>(execute);
+    }
 
-	@Override
-	public T execute() {
-		return execute.get();
-	}
+    private PteroActionImpl(Supplier<? extends T> execute) {
+        this.execute = execute;
+    }
 
-	@Override
-	public void executeAsync() {
-		this.executeAsync((result) -> {
-		});
-	}
+    @Override
+    public T execute() {
+        return execute.get();
+    }
 
-	@Override
-	public void executeAsync(Consumer<? super T> success) {
-		this.executeAsync(success, (e) -> {
-			System.err.println("An exception occured while making a request");
-			e.printStackTrace();
-		});
-	}
+    @Override
+    public void executeAsync() {
+        this.executeAsync(x -> {
+        });
+    }
 
-	@Override
-	public void executeAsync(Consumer<? super T> success, Consumer<? super Throwable> failure) {
-		CompletableFuture.supplyAsync(this::execute)
-				.whenCompleteAsync((s, f) -> {
-					try {
-						if (f == null) {
-							success.accept(s);
-						} else {
-							failure.accept(f);
-						}
-					} catch (Exception e) {
-						System.err.println("A consumer threw an exception");
-						e.printStackTrace();
-					}
-				});
-	}
+    @Override
+    public void executeAsync(Consumer<? super T> success) {
+        this.executeAsync(success, err -> {
+            System.err.println("An exception occurred while making a request");
+            err.printStackTrace();
+        });
+    }
+
+    @Override
+    public void executeAsync(Consumer<? super T> success, Consumer<? super Throwable> failure) {
+        callbackPool.submit(() -> {
+            try {
+                success.accept(execute());
+            } catch (Exception e) {
+                failure.accept(e);
+            }
+        });
+    }
 }
