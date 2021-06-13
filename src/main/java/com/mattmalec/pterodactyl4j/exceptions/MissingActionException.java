4812fff7b157b56151272aa0860917d5d84d0cba
mattmalec <matt@mattmalec.com>
2021-06-13 01:41:07 -0400

    Fix MissingActionException formatting

--- src/main/java/com/mattmalec/pterodactyl4j/exceptions/MissingActionException.java
+++ src/main/java/com/mattmalec/pterodactyl4j/exceptions/MissingActionException.java
@@ -9,12 +9,16 @@ public class MissingActionException extends RuntimeException {
     }
 
     public MissingActionException(String text, JSONObject json) {
+        super(formatMessage(text, json));
+    }
+
+    public static String formatMessage(String text, JSONObject json) {
         StringBuilder message = new StringBuilder(text + "\n\n");
         for(Object o : json.getJSONArray("errors")) {
             JSONObject obj = new JSONObject(o.toString());
             message.append("\t- ").append(obj.getString("detail")).append(" (Source: ").append(obj.getJSONObject("meta").getString("source_field")).append(")\n");
         }
-        throw new MissingActionException(message.toString());
+        return message.toString();
     }
 
     public MissingActionException(String message, Throwable cause) {
