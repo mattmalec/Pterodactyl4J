4f572d80182e78145913ba8c618809d00bb1a53f
mattmalec <matt@mattmalec.com>
2021-07-24 15:21:01 -0400

    Deprecate some PteroClient methods that can be moved to ClientServer

--- src/main/java/com/mattmalec/pterodactyl4j/client/entities/PteroClient.java
+++ src/main/java/com/mattmalec/pterodactyl4j/client/entities/PteroClient.java
@@ -8,9 +8,16 @@ import java.util.List;
 public interface PteroClient {
 
     PteroAction<Account> retrieveAccount();
+
+    @Deprecated
     PteroAction<Void> setPower(ClientServer server, PowerAction powerAction);
+
+    @Deprecated
     PteroAction<Void> sendCommand(ClientServer server, String command);
+
+    @Deprecated
     PteroAction<Utilization> retrieveUtilization(ClientServer server);
+
     PteroAction<List<ClientServer>> retrieveServers();
     PteroAction<ClientServer> retrieveServerByIdentifier(String identifier);
     PteroAction<List<ClientServer>> retrieveServersByName(String name, boolean caseSensetive);
