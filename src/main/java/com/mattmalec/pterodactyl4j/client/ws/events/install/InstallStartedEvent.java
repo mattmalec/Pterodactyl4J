620335c405e609722ad78d806ce98a76c2d4f078
mattmalec <matt@mattmalec.com>
2021-07-24 15:12:24 -0400

    Event called if skip egg script is disabled, not enabled

--- src/main/java/com/mattmalec/pterodactyl4j/client/ws/events/install/InstallStartedEvent.java
+++ src/main/java/com/mattmalec/pterodactyl4j/client/ws/events/install/InstallStartedEvent.java
@@ -4,7 +4,7 @@ import com.mattmalec.pterodactyl4j.client.entities.ClientServer;
 import com.mattmalec.pterodactyl4j.client.entities.impl.PteroClientImpl;
 import com.mattmalec.pterodactyl4j.client.managers.WebSocketManager;
 
-// this event is only called if skip egg scripts is enabled
+// this event is only called if skip egg scripts is disabled
 public class InstallStartedEvent extends InstallEvent {
 
     public InstallStartedEvent(PteroClientImpl api, ClientServer server, WebSocketManager manager, boolean installed) {
