b76238d7ef58dce6cfa130511d2691397d2b0021
mattmalec <matt@mattmalec.com>
2021-07-24 20:30:30 -0400

    Add a mappable PteroAction

---
+++ src/main/java/com/mattmalec/pterodactyl4j/requests/operators/MapPteroAction.java
@@ -0,0 +1,27 @@
+package com.mattmalec.pterodactyl4j.requests.operators;
+
+import com.mattmalec.pterodactyl4j.PteroAction;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class MapPteroAction<I, O> extends PteroActionOperator<I, O> {
+
+    private final Function<? super I, ? extends O> function;
+
+    public MapPteroAction(PteroAction<I> action, Function<? super I, ? extends O> function) {
+        super(action);
+        this.function = function;
+    }
+
+    @Override
+    public void executeAsync(Consumer<? super O> success, Consumer<? super Throwable> failure) {
+        action.executeAsync((result) -> doSuccess(success, function.apply(result)), (error) -> doFailure(failure, error));
+    }
+
+    @Override
+    public O execute(boolean shouldQueue) {
+        return function.apply(action.execute(shouldQueue));
+    }
+
+}
