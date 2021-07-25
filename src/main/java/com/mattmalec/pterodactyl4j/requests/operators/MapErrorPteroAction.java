9b206868fcb34b668e708a9f822a2f7454124a5a
mattmalec <matt@mattmalec.com>
2021-07-24 20:31:00 -0400

    Add a fallback mappable PteroAction

---
+++ src/main/java/com/mattmalec/pterodactyl4j/requests/operators/MapErrorPteroAction.java
@@ -0,0 +1,58 @@
+package com.mattmalec.pterodactyl4j.requests.operators;
+
+import com.mattmalec.pterodactyl4j.PteroAction;
+import com.mattmalec.pterodactyl4j.exceptions.PteroException;
+import com.mattmalec.pterodactyl4j.utils.ExceptionUtils;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class MapErrorPteroAction<T> extends PteroActionOperator<T, T> {
+
+    private final Predicate<? super Throwable> check;
+    private final Function<? super Throwable, ? extends T> map;
+
+    public MapErrorPteroAction(PteroAction<T> action, Predicate<? super Throwable> check, Function<? super Throwable, ? extends T> map) {
+        super(action);
+        this.check = check;
+        this.map = map;
+    }
+
+    @Override
+    public void executeAsync(Consumer<? super T> success, Consumer<? super Throwable> failure) {
+        action.executeAsync(success, (error) -> {
+            try {
+                if (check.test(error))
+                    doSuccess(success, map.apply(error));
+                else
+                    doFailure(failure, error);
+            } catch (Throwable e) {
+                doFailure(failure, ExceptionUtils.appendCause(e, error));
+            }
+        });
+    }
+
+    @Override
+    public T execute(boolean shouldQueue) {
+        try {
+            return action.execute(shouldQueue);
+        } catch (Throwable error) {
+            try {
+                if (check.test(error))
+                    return map.apply(error);
+            } catch (Throwable e) {
+                fail(ExceptionUtils.appendCause(e, error));
+            }
+            fail(error);
+        }
+        throw new AssertionError("Unreachable");
+    }
+
+    private void fail(Throwable error) {
+        if (error instanceof PteroException)
+            throw (PteroException) error;
+        else
+            throw new RuntimeException(error);
+    }
+}
