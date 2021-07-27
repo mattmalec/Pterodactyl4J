86af847af4746d03ce14bfc11ca8f95a8de9cc92
mattmalec <matt@mattmalec.com>
2021-07-27 17:40:58 -0400

    Add documentation for WebSocketBuilder

--- src/main/java/com/mattmalec/pterodactyl4j/client/managers/WebSocketBuilder.java
+++ src/main/java/com/mattmalec/pterodactyl4j/client/managers/WebSocketBuilder.java
@@ -18,6 +18,7 @@ package com.mattmalec.pterodactyl4j.client.managers;
 
 import com.mattmalec.pterodactyl4j.client.entities.ClientServer;
 import com.mattmalec.pterodactyl4j.client.entities.impl.PteroClientImpl;
+import com.mattmalec.pterodactyl4j.client.ws.events.Event;
 import com.mattmalec.pterodactyl4j.client.ws.hooks.IClientListenerManager;
 import com.mattmalec.pterodactyl4j.client.ws.hooks.InterfacedClientListenerManager;
 
@@ -27,6 +28,8 @@ public class WebSocketBuilder {
     private final PteroClientImpl api;
     private final ClientServer server;
 
+    private boolean useFreshServer;
+
     public WebSocketBuilder(PteroClientImpl api, ClientServer server) {
         this.api = api;
         this.server = server;
@@ -34,6 +37,16 @@ public class WebSocketBuilder {
     }
 
 
+    /**
+     * Registers the specified listeners
+     *
+     * @param listeners
+     *        The listener objects
+     *
+     * @throws java.lang.IllegalArgumentException
+     *         If one of the listeners does not implement {@link com.mattmalec.pterodactyl4j.client.ws.hooks.ClientSocketListener ClientSocketListener}
+     *         or if the listeners are null
+     */
     public WebSocketBuilder addEventListeners(Object...  listeners) {
         if (listeners == null)
             throw new IllegalArgumentException("Listeners cannot be null");
@@ -44,6 +57,15 @@ public class WebSocketBuilder {
         return this;
     }
 
+    /**
+     * Removes the specified listeners
+     *
+     * @param listeners
+     *        The listener objects to remove
+     *
+     * @throws java.lang.IllegalArgumentException
+     *         If the listeners are null
+     */
     public WebSocketBuilder removeEventListeners(Object...  listeners) {
         if (listeners == null)
             throw new IllegalArgumentException("Listeners cannot be null");
@@ -54,8 +76,41 @@ public class WebSocketBuilder {
         return this;
     }
 
+    /**
+     * Enables/Disables P4J to return the latest server instance when firing events.
+     *
+     * <p>By default, the {@link com.mattmalec.pterodactyl4j.client.entities.ClientServer ClientServer}
+     * provided in the event context will be the <b>same instance</b> when the WebSocket was built. Generally, this behavior is preferred
+     * since you can use {@link Event#retrieveServer()} when you need an updated instance.
+     *
+     * <p>However, if your program requires the latest data from the {@link com.mattmalec.pterodactyl4j.client.entities.ClientServer ClientServer},
+     * you can use this method to have P4J retrieve the server, then fire the event using the new data.
+     *
+     * <p><b>Note:</b> If this is enabled, and the panel rate limit is hit, events will stop firing until the limit is lifted
+     *
+     * <p>Default: <b>false (disabled)</b>
+     *
+     * @param  enable
+     *         True - provide a fresh instance of the {@link com.mattmalec.pterodactyl4j.client.entities.ClientServer ClientServer}
+     *         in event context
+     *
+     * @return The {@link com.mattmalec.pterodactyl4j.client.managers.WebSocketBuilder WebSocketBuilder} instance. Useful for chaining.
+     */
+    public WebSocketBuilder setEnableFreshServer(boolean enable) {
+        this.useFreshServer = enable;
+        return this;
+    }
+
+    /**
+     * Builds a new {@link com.mattmalec.pterodactyl4j.client.managers.WebSocketManager WebSocketManager}
+     * instance and retrieves a token from the panel to start the authorization process.
+     * <p>The authorization process runs in a different thread, so while this will return immediately, the manager has not
+     * finished connecting.
+     *
+     * @return A {@link com.mattmalec.pterodactyl4j.client.managers.WebSocketManager WebSocketManager} that is ready for use
+     */
     public WebSocketManager build() {
-        return new WebSocketManager(api, server, eventManager);
+        return new WebSocketManager(api, server, eventManager, useFreshServer);
     }
 
 
