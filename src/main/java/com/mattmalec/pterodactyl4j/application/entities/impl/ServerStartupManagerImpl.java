547ea9248b2aba5dc38e4c2fa32b850fce6f9856
mattmalec <matt@mattmalec.com>
2021-08-08 22:04:51 -0400

    Create ServerStartupManager and implementation

---
+++ src/main/java/com/mattmalec/pterodactyl4j/application/entities/impl/ServerStartupManagerImpl.java
@@ -0,0 +1,110 @@
+/*
+ *    Copyright 2021 Matt Malec, and the Pterodactyl4J contributors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.mattmalec.pterodactyl4j.application.entities.impl;
+
+import com.mattmalec.pterodactyl4j.EnvironmentValue;
+import com.mattmalec.pterodactyl4j.application.entities.ApplicationEgg;
+import com.mattmalec.pterodactyl4j.application.entities.ApplicationServer;
+import com.mattmalec.pterodactyl4j.application.managers.ServerStartupManager;
+import com.mattmalec.pterodactyl4j.requests.Route;
+import com.mattmalec.pterodactyl4j.requests.action.AbstractManagerBase;
+import com.mattmalec.pterodactyl4j.utils.Checks;
+import okhttp3.RequestBody;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerStartupManagerImpl extends AbstractManagerBase implements ServerStartupManager {
+
+    private final ApplicationServer server;
+
+    private String startupCommand;
+    private Map<String, String> environment;
+    private long egg;
+    private String image;
+    private boolean skipScripts;
+
+    public ServerStartupManagerImpl(ApplicationServer server, PteroApplicationImpl impl) {
+        super(impl, Route.Servers.UPDATE_SERVER_STARTUP.compile(server.getId()));
+        this.server = server;
+    }
+
+    @Override
+    public ServerStartupManager setStartupCommand(String command) {
+        Checks.notNull(command, "Startup Command");
+
+        set |= STARTUP_COMMAND;
+        this.startupCommand = command;
+        return this;
+    }
+
+    @Override
+    public ServerStartupManager setEnvironment(Map<String, EnvironmentValue<?>> environment) {
+        Checks.notNull(environment, "Environment");
+
+        Map<String, EnvironmentValue<?>> env = new HashMap<>(server.getContainer().getEnvironment());
+        env.putAll(environment);
+
+        set |= ENVIRONMENT;
+        this.environment = env.entrySet().stream().collect(EnvironmentValue.collector());
+        return this;
+    }
+
+    @Override
+    public ServerStartupManager setEgg(ApplicationEgg egg) {
+        Checks.notNull(environment, "Egg");
+
+        set |= EGG;
+        this.egg = egg.getIdLong();
+        return this;
+    }
+
+    @Override
+    public ServerStartupManager setImage(String dockerImage) {
+        Checks.notNull(dockerImage, "Docker Image");
+        Checks.check(dockerImage.length() <= 191, "Docker image cannot be longer than 191 characters");
+
+        set |= IMAGE;
+        this.image = dockerImage;
+        return this;
+    }
+
+    @Override
+    public ServerStartupManager setSkipScripts(boolean skipScripts) {
+        set |= SKIP_SCRIPTS;
+        this.skipScripts = skipScripts;
+        return this;
+    }
+
+    @Override
+    protected RequestBody finalizeData() {
+
+        JSONObject obj = new JSONObject()
+                .put("startup", shouldUpdate(STARTUP_COMMAND) ? startupCommand : server.getContainer().getStartupCommand())
+                .put("environment", shouldUpdate(ENVIRONMENT) ? environment : server.getContainer().getEnvironment().entrySet()
+                        .stream().collect(EnvironmentValue.collector()))
+                .put("egg", shouldUpdate(EGG) ? egg : server.getEggIdLong())
+                .put("image", shouldUpdate(IMAGE) ? image : server.getContainer().getImage())
+
+                // this won't do anything since the server is installed, but pterodactyl requires it so here we are
+                .put("skip_scripts", !shouldUpdate(SKIP_SCRIPTS) || skipScripts);
+
+        super.reset();
+        return getRequestBody(obj);
+    }
+}
---
+++ src/main/java/com/mattmalec/pterodactyl4j/application/managers/ServerStartupManager.java
@@ -0,0 +1,131 @@
+/*
+ *    Copyright 2021 Matt Malec, and the Pterodactyl4J contributors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.mattmalec.pterodactyl4j.application.managers;
+
+import com.mattmalec.pterodactyl4j.EnvironmentValue;
+import com.mattmalec.pterodactyl4j.PteroAction;
+import com.mattmalec.pterodactyl4j.application.entities.ApplicationEgg;
+import com.mattmalec.pterodactyl4j.application.entities.ApplicationServer;
+import com.mattmalec.pterodactyl4j.client.entities.ClientServer;
+
+import java.util.Map;
+
+/**
+ * Manager providing functionality to modify the startup configuration for an
+ * {@link ApplicationServer ApplicationServer}.
+ *
+ * <p><b>Example</b>
+ * <pre>{@code
+ * manager.setEgg(egg) // set the egg for the server
+ *     .setImage("ghcr.io/pterodactyl/yolks:java_8") // set the docker image to yolks
+ *     .executeAsync();
+ * }</pre>
+ *
+ * @see ApplicationServer#getStartupManager()
+ */
+public interface ServerStartupManager extends PteroAction<ApplicationServer> {
+
+    // The following bits are used internally, but are here in case something user-facing is added in the future
+    
+    long STARTUP_COMMAND = 0x1;
+    long ENVIRONMENT = 0x2;
+    long EGG = 0x4;
+    long IMAGE = 0x8;
+    long SKIP_SCRIPTS = 0x10;
+
+    /**
+     * Sets the start up command of this {@link com.mattmalec.pterodactyl4j.application.entities.ApplicationServer ApplicationServer}.
+     *
+     * @param  command
+     *         The new startup command for the server
+     *
+     * @throws IllegalArgumentException
+     *         If the provided command is {@code null}
+     *
+     * @return The {@link com.mattmalec.pterodactyl4j.application.managers.ServerStartupManager ServerStartupManager}
+     * instance, useful for chaining
+     */
+    ServerStartupManager setStartupCommand(String command);
+
+    /**
+     * Sets the egg environment variables of this {@link com.mattmalec.pterodactyl4j.application.entities.ApplicationServer ApplicationServer}.
+     * <br>This method will not remove any existing environment variables, it will only replace them.
+     *
+     * @param  environment
+     *         The updated map
+     *
+     * @throws IllegalArgumentException
+     *         If the provided environment map is {@code null}
+     *
+     * @return The {@link com.mattmalec.pterodactyl4j.application.managers.ServerStartupManager ServerStartupManager}
+     * instance, useful for chaining
+     *
+     * @see EnvironmentValue
+     */
+    ServerStartupManager setEnvironment(Map<String, EnvironmentValue<?>> environment);
+
+    /**
+     * Sets the egg to use with this {@link com.mattmalec.pterodactyl4j.application.entities.ApplicationServer ApplicationServer}.
+     *
+     * <p><b>Note:</b> You will need to restart the server for the changes to take effect.
+     *
+     * @param  egg
+     *         The new egg for the server
+     *
+     * @throws IllegalArgumentException
+     *         If the provided egg is {@code null}
+     *
+     * @return The {@link com.mattmalec.pterodactyl4j.application.managers.ServerStartupManager ServerStartupManager}
+     * instance, useful for chaining
+     *
+     * @see ClientServer#restart()
+     */
+    ServerStartupManager setEgg(ApplicationEgg egg);
+
+    /**
+     * Sets the Docker image for the {@link com.mattmalec.pterodactyl4j.application.entities.ApplicationServer ApplicationServer} to use.
+     *
+     * <p><b>Note:</b> You will need to restart the server for the changes to take effect.
+     *
+     * @param  dockerImage
+     *         The new Docker image for the server
+     *
+     * @throws IllegalArgumentException
+     *         If the provided image is {@code null} or longer then 191 characters
+     *
+     * @return The {@link com.mattmalec.pterodactyl4j.application.managers.ServerStartupManager ServerStartupManager}
+     * instance, useful for chaining
+     *
+     * @see ClientServer#restart()
+     */
+    ServerStartupManager setImage(String dockerImage);
+
+    /**
+     * Enables/Disables Pterodactyl to run the egg install scripts for {@link com.mattmalec.pterodactyl4j.application.entities.ApplicationServer ApplicationServer}.
+     * <p><b>Note:</b> This will have no effect on a server that's already installed, so it's here for consistancy.
+     *
+     * <p>Default: <b>disabled (false)</b>
+     *
+     * @param  skipScripts
+     *         True - will not run egg install scripts
+     *
+     * @return The {@link com.mattmalec.pterodactyl4j.application.managers.ServerStartupManager ServerStartupManager}
+     * instance, useful for chaining
+     */
+    ServerStartupManager setSkipScripts(boolean skipScripts);
+
+}
