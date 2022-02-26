2f2586b1305d0b2cbe3cfb4f320bb14258edaac8
Matt Malec <matt@mattmalec.com>
2022-02-26 15:48:56 -0500

    Refactor AllocationManager into ApplicationAllocationManager

--- src/main/java/com/mattmalec/pterodactyl4j/application/managers/ApplicationAllocationManager.java
+++ src/main/java/com/mattmalec/pterodactyl4j/application/managers/ApplicationAllocationManager.java
@@ -17,11 +17,11 @@
 package com.mattmalec.pterodactyl4j.application.managers;
 
 import com.mattmalec.pterodactyl4j.PteroAction;
-import com.mattmalec.pterodactyl4j.application.entities.Allocation;
+import com.mattmalec.pterodactyl4j.application.entities.ApplicationAllocation;
 
-public interface AllocationManager {
+public interface ApplicationAllocationManager {
 
     AllocationAction createAllocation();
-    PteroAction<Void> deleteAllocation(Allocation allocation);
+    PteroAction<Void> deleteAllocation(ApplicationAllocation allocation);
 
 }
