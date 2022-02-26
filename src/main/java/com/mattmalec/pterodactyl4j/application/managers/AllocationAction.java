1eea2230cc54aec5bda26c0c21e6b240c3066bb2
Matt Malec <matt@mattmalec.com>
2022-02-26 15:47:38 -0500

    Remove these worthless AllocationAction functions

--- src/main/java/com/mattmalec/pterodactyl4j/application/managers/AllocationAction.java
+++ src/main/java/com/mattmalec/pterodactyl4j/application/managers/AllocationAction.java
@@ -23,9 +23,5 @@ public interface AllocationAction extends PteroAction<Void> {
     AllocationAction setIP(String ip);
     AllocationAction setAlias(String alias);
     AllocationAction setPorts(String... ports);
-    AllocationAction addPorts(String... ports);
-    AllocationAction addPort(String port);
-    AllocationAction removePort(String port);
-    AllocationAction removePorts(String... ports);
 
 }
