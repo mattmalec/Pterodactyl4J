eaec7ad829acc10f28a0ef4427e431c5a14dc733
Matt Malec <matt@mattmalec.com>
2022-04-16 20:47:50 -0400

    Add documentation to FailureEvent

--- src/main/java/com/mattmalec/pterodactyl4j/client/ws/events/connection/FailureEvent.java
+++ src/main/java/com/mattmalec/pterodactyl4j/client/ws/events/connection/FailureEvent.java
@@ -24,6 +24,11 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+/**
+ * Indicates that P4J encountered a Throwable that could not be forwarded to another end-user frontend.
+ * <br>This is fired for events in internal WebSocket handling.
+ * This normally includes subtypes of {@link java.io.IOException IOExceptions}
+ */
 public class FailureEvent extends ConnectionEvent {
 
     private final Response response;
@@ -35,10 +40,31 @@ public class FailureEvent extends ConnectionEvent {
         this.throwable = throwable;
     }
 
+    /**
+     * The cause Throwable for this event.
+     * <br>There are several types of Throwable instances that can be expected here.
+     *
+     * <p>For instance, if the Throwable is a type of {@link java.io.EOFException EOFException}, that means P4J unexpectedly
+     * lost connection to Wings.
+     *
+     * <p>If the Throwable is a type of {@link java.net.ConnectException ConnectException}, that means P4J was not
+     * able to reach Wings
+     *
+     * @return The cause
+     */
     public Throwable getThrowable() {
         return throwable;
     }
 
+    /**
+     * Read the response body of the event, deserialized as a {@link org.json.JSONObject JSONObject}
+     * <br>There normally isn't a case when calling this method is necessary.
+     *
+     * @throws IOException
+     *         If the provided response body cannot be deserialized
+     *
+     * @return The response body
+     */
     public JSONObject getResponse() throws IOException {
         return new JSONObject(response.body().string());
     }
