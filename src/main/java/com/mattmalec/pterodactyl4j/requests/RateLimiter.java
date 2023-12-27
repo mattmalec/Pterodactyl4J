f1d1eb96985686fc68ce23e1bd46ee85d1601fc6
RaftDev <Theraft08@gmail.com>
2023-12-27 18:59:49 +0100

    RateLimiter removes request from queue when failed

--- src/main/java/com/mattmalec/pterodactyl4j/requests/RateLimiter.java
+++ src/main/java/com/mattmalec/pterodactyl4j/requests/RateLimiter.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2021-2022 Matt Malec, and the Pterodactyl4J contributors
+ *    Copyright 2021-2023 Matt Malec, and the Pterodactyl4J contributors
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -156,6 +156,7 @@ public class RateLimiter implements Runnable {
 			} catch (Exception ex) {
 				RATELIMIT_LOG.error("Encountered exception trying to execute request");
 				ex.printStackTrace();
+				iterator.remove();
 				break;
 			}
 		}
